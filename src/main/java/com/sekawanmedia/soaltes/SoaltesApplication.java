cb4348b5e7f68f2abb2c67d8a56853980a27eccc
adinlamongan <jmajid7@gmail.com>
2023-07-27 16:15:49 +0700

    update

--- src/main/java/com/sekawanmedia/soaltes/SoaltesApplication.java
+++ src/main/java/com/sekawanmedia/soaltes/SoaltesApplication.java
@@ -2,21 +2,12 @@ package com.sekawanmedia.soaltes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-// import springfox.documentation.builders.RequestHandlerSelectors;
-// import springfox.documentation.spi.DocumentationType;
-// import springfox.documentation.spring.web.plugins.Docket;
-// import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 
 @SpringBootApplication
-//@EnableSwagger2
 public class SoaltesApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SoaltesApplication.class, args);
 	}
-
-	// public Docket apis(){
-	// 	return new Docket(DocumentationType.SWAGGER_2).select().apis(RequestHandlerSelectors.any()).build();
-	// }
-
 }
